7cd9ee1fd103d25cfb8d958a177ee5b9997f1a4c
RaulFit <fitraul6@gmail.com>
2023-04-07 17:00:31 +0300

    Test 7 working

--- Json.Validator/JsonString.cs
+++ Json.Validator/JsonString.cs
@@ -4,9 +4,11 @@ namespace Json
 {
     public static class JsonString
     {
+        const int FirstNonUnicodeChar = 32;
+
         public static bool IsJsonString(string input)
         {
-            return HasContent(input) && IsDoubleQuoted(input);
+            return HasContent(input) && IsDoubleQuoted(input) && !ContainsControlCharacters(input);
         }
 
         public static bool IsDoubleQuoted(string input)
@@ -17,6 +19,19 @@ namespace Json
         public static bool HasContent(string input)
         {
             return !string.IsNullOrEmpty(input);
+        }
+
+        public static bool ContainsControlCharacters(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] < FirstNonUnicodeChar)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }

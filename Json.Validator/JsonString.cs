d31523c6cb689df97bcbb61c969384cfacf4dd7f
RaulFit <fitraul6@gmail.com>
2023-04-09 20:43:29 +0300

    String tests working.

--- Json.Validator/JsonString.cs
+++ Json.Validator/JsonString.cs
@@ -7,6 +7,9 @@ namespace Json
     public static class JsonString
     {
         const int FirstNonUnicodeChar = 32;
+        const int HexaDigits = 4;
+        const string EscapeChars = "\"\\/bfnrtu";
+        const string Hexa = "0123456789ABCDEFabcdef";
 
         public static bool IsJsonString(string input)
         {
@@ -38,12 +41,11 @@ namespace Json
 
         public static bool ContainsUnrecognizedEscapeCharacters(string input)
         {
-            const string escapeChars = "\"\\/bfnrtu";
             for (int i = 0; i < input.Length; ++i)
             {
                 if (input[i] == '\\')
                 {
-                    if (i < input.Length - 1 && escapeChars.Contains(input[i + 1]))
+                    if (i < input.Length - 1 && EscapeChars.Contains(input[i + 1]))
                     {
                         i++;
                         continue;
@@ -63,23 +65,21 @@ namespace Json
 
         public static bool ContainsUnfinishedHexNumber(string input)
         {
-            const int hexaDigits = 4;
             int lastPossibleUnicode = input.Length - 2;
-            const string hexa = "0123456789ABCDEFabcdef";
             for (int i = 0; i < lastPossibleUnicode; ++i)
             {
                 if (input[i] == '\\' && input[i + 1] == 'u')
                 {
-                    if (input.Length - 1 - i < hexaDigits)
+                    if (input.Length - 1 - i < HexaDigits)
                     {
                         return true;
                     }
 
                     i++;
 
-                    for (int j = i + 1; j <= i + hexaDigits; j++)
+                    for (int j = i + 1; j <= i + HexaDigits; j++)
                     {
-                        if (!hexa.Contains(input[j]))
+                        if (!Hexa.Contains(input[j]))
                         {
                             return true;
                         }

ab9e57d51d4f8352f8b87b15ee4ccc5a2b225401
RaulFit <fitraul6@gmail.com>
2023-04-24 21:00:02 +0300

    Modified json number

--- Json.Validator/JsonNumber.cs
+++ Json.Validator/JsonNumber.cs
@@ -6,8 +6,9 @@ namespace Json
 {
     public static class JsonNumber
     {
-        const string Exponent = "e";
         const string Operators = "+-";
+        const string UnfinishedExponent = "e+-";
+        const int ExponentHasOperatorStart = 2;
 
         public static bool IsJsonNumber(string input)
         {
@@ -20,44 +21,34 @@ namespace Json
             int dotIndex = input.IndexOf('.');
             int exponentIndex = input.IndexOf('e');
 
-            return IsInteger(GetInteger(input, dotIndex, exponentIndex)) && IsFraction(GetFraction(input, dotIndex, exponentIndex)) && IsExponent(GetExponent(input, exponentIndex));
+            return IsInteger(GetInteger(input, dotIndex, exponentIndex))
+                && IsFraction(GetFraction(input, dotIndex, exponentIndex))
+                && IsExponent(GetExponent(input, exponentIndex));
         }
 
         public static bool HasValidDigits(string input)
         {
             for (int i = 0; i < input.Length; i++)
             {
-                if ((input[i] < '0' || input[i] > '9') && input[i] != 'e')
+                if (input[i] < '0' || input[i] > '9')
                 {
                     return false;
                 }
             }
 
-            return true;
+            return input.Length > 0;
         }
 
         public static string GetInteger(string input, int dotIndex, int exponentIndex)
         {
-            string fraction = GetFraction(input, dotIndex, exponentIndex);
-
-            if (fraction.Length == 0 && input.Contains('.'))
-            {
-                return "Wrong";
-            }
-
-            if (input.Length > 1 && input.StartsWith('0') && !input.Contains('.'))
-            {
-                return "Wrong";
-            }
-
             if (dotIndex != -1)
             {
-                return input.Substring(0, dotIndex);
+                return input[..dotIndex];
             }
 
             if (exponentIndex != -1)
             {
-                return input.Substring(0, exponentIndex);
+                return input[..exponentIndex];
             }
 
             return input;
@@ -65,26 +56,29 @@ namespace Json
 
         public static bool IsInteger(string input)
         {
-            int i = 0;
-
-            if (input[i] == '-')
+            if (input[0] == '-')
             {
-                i++;
+                input = input[1..];
             }
 
-            if (input[i] == '0' && input.Length - i > 1)
+            if (input.StartsWith('0') && input.Length > 1)
             {
                 return false;
             }
 
-            return HasValidDigits(input.Substring(i)) && !input.Substring(i).Contains('e');
+            return HasValidDigits(input);
         }
 
         public static string GetFraction(string input, int dotIndex, int exponentIndex)
         {
+            if (dotIndex != -1 && exponentIndex != -1)
+            {
+                return input[dotIndex..exponentIndex];
+            }
+
             if (dotIndex != -1)
             {
-                return input.Substring(dotIndex + 1);
+                return input[dotIndex..];
             }
 
             return string.Empty;
@@ -92,42 +86,14 @@ namespace Json
 
         public static bool IsFraction(string input)
         {
-            if (string.IsNullOrEmpty(input))
-            {
-                return true;
-            }
-
-            if (input.Contains("."))
-            {
-                return false;
-            }
-
-            if (!input.Contains('e'))
-            {
-                return HasValidDigits(input);
-            }
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                if ((input[i] < '0' || input[i] > '9') && !Operators.Contains(input[i]) && !Exponent.Contains(input[i]))
-                {
-                    return false;
-                }
-            }
-
-            return true;
+            return input == string.Empty || HasValidDigits(input[1..]);
         }
 
         public static string GetExponent(string input, int exponentIndex)
         {
             if (exponentIndex != -1)
             {
-                if (input[exponentIndex - 1] < '0' || input[exponentIndex - 1] > '9')
-                {
-                    return "Wrong";
-                }
-
-                return input.Substring(exponentIndex);
+                return input[exponentIndex..];
             }
 
             return string.Empty;
@@ -135,32 +101,22 @@ namespace Json
 
         public static bool IsExponent(string input)
         {
-            if (string.IsNullOrEmpty(input))
+            if (input == string.Empty)
             {
                 return true;
             }
 
-            if (Exponent.Contains(input[input.Length - 1]) || Operators.Contains(input[input.Length - 1]))
+            if (UnfinishedExponent.Contains(input[input.Length - 1]))
             {
                 return false;
             }
 
-            int operators = 0;
-
-            for (int i = 1; i < input.Length; i++)
+            if (Operators.Contains(input[1]))
             {
-                if ((input[i] < '0' || input[i] > '9') && !Operators.Contains(input[i]))
-                {
-                    return false;
-                }
-
-                if (Operators.Contains(input[i]))
-                {
-                    operators++;
-                }
+                return HasValidDigits(input[ExponentHasOperatorStart..]);
             }
 
-            return operators <= 1;
+            return HasValidDigits(input[1..]);
         }
     }
 }

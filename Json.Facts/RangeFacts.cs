dca4738f6440b7ccd56ceff8fbba3dabdc8eced3
RaulFit <fitraul6@gmail.com>
2023-05-04 22:17:54 +0300

    Added class range and tests.

---
+++ Json.Facts/RangeFacts.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace Json.Facts
+{
+    public class RangeFacts
+    {
+        [Fact]
+        public void FirstCharacterCanBeEqualToStart()
+        {
+            var digit = new Range('a', 'f');
+            Assert.True(digit.Match("abc"));
+        }
+
+        [Fact]
+        public void FirstCharacterCanBeEqualToEnd()
+        {
+            var digit = new Range('a', 'f');
+            Assert.True(digit.Match("fab"));
+        }
+
+        [Fact]
+        public void FirstCharacterCanBeBetweenStartAndEnd()
+        {
+            var digit = new Range('a', 'f');
+            Assert.True(digit.Match("bcd"));
+        }
+
+        [Fact]
+        public void FirstCharacterIsNotInInterval()
+        {
+            var digit = new Range('a', 'f');
+            Assert.False(digit.Match("1ab"));
+        }
+
+        [Fact]
+        public void StringCannotBeNull()
+        {
+            var digit = new Range('a', 'f');
+            Assert.False(digit.Match(null));
+        }
+    }
+}

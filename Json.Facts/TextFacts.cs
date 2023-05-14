692982c11946f90a75e5a239ab88f57dc77122cd
RaulFit <fitraul6@gmail.com>
2023-05-15 00:31:52 +0300

    Added class Text and TextFacts.

---
+++ Json.Facts/TextFacts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Json.Facts
+{
+    public class TextFacts
+    {
+        [Theory]
+        [InlineData("true", "true", "")]
+        [InlineData("true", "trueX", "X")]
+        [InlineData("true", "false", "false")]
+        [InlineData("true", "", "")]
+        [InlineData("true", null, null)]
+        [InlineData("false", "false", "")]
+        [InlineData("false", "falseX", "X")]
+        [InlineData("false", "true", "true")]
+        [InlineData("false", "", "")]
+        [InlineData("false", null, null)]
+        [InlineData("", "true", "true")]
+        [InlineData("", null, null)]
+
+        public void CheckIfTextStartsWithPrefix(string prefix, string text, string result)
+        {
+            var pref = new Text(prefix);
+            Assert.Equal(result, pref.Match(text).RemainingText());
+        }
+    }
+}
---
+++ Json.Validator/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Text : IPattern
+    {
+        private readonly string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text) || !text.StartsWith(prefix))
+            {
+                return new Match(false, text);
+            }
+
+            return new Match(true, text[prefix.Length..]);
+        }
+    }
+}

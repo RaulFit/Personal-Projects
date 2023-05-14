b485f5c34fd3081846ba6a4358f4896530ee171b
FitRaul <91602738+FitRaul@users.noreply.github.com>
2023-05-14 14:41:03 +0300

    Added Facts for class Any

---
+++ Json.Facts/AnyFacts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Json.Facts
+{
+    public class AnyFacts
+    {
+        [Theory]
+        [InlineData("eE", "ea", "a")]
+        [InlineData("eE", "Ea", "a")]
+        [InlineData("eE", "a", "a")]
+        [InlineData("eE", "", "")]
+        [InlineData("eE", null, null)]
+        
+        public void ClassAnyWorksOnLetters(string constructorText, string matchText, string result)
+        {
+            var any = new Any(constructorText);
+            Assert.Equal(result, any.Match(matchText).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("-+", "+3", "3")]
+        [InlineData("-+", "-2", "2")]
+        [InlineData("-+", "2", "2")]
+        [InlineData("-+", "", "")]
+        [InlineData("-+", null, null)]
+
+        public void ClassAnyWorksOnOperators(string constructorText, string matchText, string result)
+        {
+            var any = new Any(constructorText);
+            Assert.Equal(result, any.Match(matchText).RemainingText());
+        }
+    }
+}
---
+++ Json.Validator/Any.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Any : IPattern
+    {
+        private readonly string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text) || !this.accepted.Contains(text[0]))
+            {
+                return new Match(false, text);
+            }
+
+            return new Match(true, text[1..]);
+        }
+    }
+}

e8701a0b2b976fe4d15285c9d04e9bc46a9b8f51
RaulFit <fitraul6@gmail.com>
2023-05-15 18:21:10 +0300

    Added class Many and ManyFacts

---
+++ Json.Facts/ManyFacts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Json.Facts
+{
+    public class ManyFacts
+    {
+        [Theory]
+        [InlineData("abc", "bc")]
+        [InlineData("aaaabc", "bc")]
+        [InlineData("bc", "bc")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void ManyWorksOnClassCharacter(string text, string result)
+        {
+            var a = new Many(new Character('a'));
+            Assert.Equal(result, a.Match(text).RemainingText());
+        }
+
+        [Theory]
+        [InlineData("12345ab123", "ab123")]
+        [InlineData("ab", "ab")]
+      
+        public void ManyWorksOnClassRange(string text, string result)
+        {
+            var digits = new Many(new Range('0', '9'));
+            Assert.Equal(result, digits.Match(text).RemainingText());
+        }
+    }
+}
---
+++ Json.Validator/Many.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Many
+    {
+        private readonly IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            IMatch match = new Match(true, text);
+            while (!string.IsNullOrEmpty(text) && pattern.Match(match.RemainingText()[0].ToString()).Success())
+            {
+                match = pattern.Match(match.RemainingText());
+            }
+
+            return match;
+        }
+    }
+}

2eb01a4b8f96f0f2cf5881937aee99be3e853113
RaulFit <fitraul6@gmail.com>
2023-06-06 16:06:58 +0300

    Modified class value

--- Json.Validator/Value.cs
+++ Json.Validator/Value.cs
@@ -11,15 +11,30 @@ namespace Json
 
         public Value()
         {
-            var ws = new Optional(new Choice(new Character('\u0020'), new Character('\u000A'), new Character('\u000D'), new Character('\u0009')));
+            var ws = new Optional(new Any("\b\n\r\t"));
             var value = new Choice(new String(), new Number(), new Text("true"), new Text("false"), new Text("null"));
             var element = new Sequence(ws, value, ws);
-            var elements = new List(element, new Text(", "));
+            var elements = new List(element, new Character(','));
             var member = new Sequence(ws, new String(), ws, new Character(':'), element);
             var members = new List(member, new Character(','));
-            var obj = new Choice(new Sequence(new Character('{'), ws, new Character('}')), new Sequence(new Character('{'), members, new Character('}')));
-            var array = new Choice(new Sequence(new Character('['), ws, new Character(']')), new Sequence(new Character('['), elements, new Character(']')));
-            this.pattern = new Choice(obj, array, value);
+            var obj = new Sequence(new Character('{'), ws, members, ws, new Character('}'));
+            var array = new Sequence(new Character('['), ws, elements, ws, new Character(']'));
+            var jsonValue = new Choice(value, obj, array);
+            this.pattern = new Sequence(
+                new Text("[\n"),
+                ws,
+                new OneOrMore(new Sequence(
+                new Text("{\n"),
+                ws,
+                new List(
+                    new Sequence(
+                        new String(),
+                        new Character(':'),
+                        ws,
+                        jsonValue),
+                    new Text(",\n\t")),
+                new Sequence(new Text("\n\t}"), new Optional(new Text(",\n"))))),
+                new Text("\n]"));
         }
 
         public IMatch Match(string text)

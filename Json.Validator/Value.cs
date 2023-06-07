1b5c6d3da7d8d11a7150b4e7ead134bd3cbe81f4
RaulFit <fitraul6@gmail.com>
2023-06-07 14:03:39 +0300

    Modified class Value

--- Json.Validator/Value.cs
+++ Json.Validator/Value.cs
@@ -11,7 +11,7 @@ namespace Json
 
         public Value()
         {
-            var ws = new Optional(new Any("\b\n\r\t"));
+            var ws = new Many(new Any("\b\n\r\t"));
             var value = new Choice(new String(), new Number(), new Text("true"), new Text("false"), new Text("null"));
             var element = new Sequence(ws, value, ws);
             var elements = new List(element, new Character(','));
@@ -21,9 +21,9 @@ namespace Json
             var array = new Sequence(new Character('['), ws, elements, ws, new Character(']'));
             this.pattern = new Sequence(
                 new Character('{'),
-                new Many(ws),
-                new List(new Sequence(new String(), new Text(": "), new Choice(value, obj, array)), new Sequence(new Text(",\n"), new Many(ws))),
-                new Many(ws),
+                ws,
+                new List(new Sequence(new String(), new Character(':'), ws, new Choice(value, obj, array)), new Sequence(new Character(','), ws)),
+                ws,
                 new Character('}'));
         }
 

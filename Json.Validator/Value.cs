9a5b1762eca243a6bf4e0573e373bdf83dbe379e
RaulFit <fitraul6@gmail.com>
2023-06-07 15:55:04 +0300

    Modified class Value

--- Json.Validator/Value.cs
+++ Json.Validator/Value.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Json
 {
@@ -11,7 +12,7 @@ namespace Json
 
         public Value()
         {
-            var ws = new Many(new Any("\b\n\r\t"));
+            var ws = new Many(new Any(" \n\r\t"));
             var value = new Choice(new String(), new Number(), new Text("true"), new Text("false"), new Text("null"));
             var element = new Sequence(ws, value, ws);
             var elements = new List(element, new Character(','));
@@ -19,10 +20,13 @@ namespace Json
             var members = new List(member, new Character(','));
             var obj = new Sequence(new Character('{'), ws, members, ws, new Character('}'));
             var array = new Sequence(new Character('['), ws, elements, ws, new Character(']'));
+            var val = new Choice(new String(), new Number(), new Text("true"), new Text("false"), new Text("null"));
+            val.Add(array);
+            val.Add(obj);
             this.pattern = new Sequence(
                 new Character('{'),
                 ws,
-                new List(new Sequence(new String(), new Character(':'), ws, new Choice(value, obj, array)), new Sequence(new Character(','), ws)),
+                new List(new Sequence(new String(), new Character(':'), ws, val), new Sequence(new Character(','), ws)),
                 ws,
                 new Character('}'));
         }

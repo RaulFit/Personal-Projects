df92dcc9b096a252a3a776ceb47617f6ffae78de
RaulFit <fitraul6@gmail.com>
2023-06-10 21:24:18 +0300

    Added JsonValidator

---
+++ JSONValidator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class JSONValidator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}

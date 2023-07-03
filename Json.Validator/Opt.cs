1a50443c94ced0d90499fa7419f531c8f69a4df0
RaulFit <fitraul6@gmail.com>
2023-07-03 17:45:36 +0300

    Modified Match

---
+++ Json.Validator/Opt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Opt : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Opt(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return new Match(true, pattern.Match(text).RemainingText());
+        }
+    }
+}

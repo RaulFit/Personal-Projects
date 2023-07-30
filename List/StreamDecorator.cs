b47aee008c7d798c0a007c767109dd4875a237ed
RaulFit <fitraul6@gmail.com>
2023-07-30 22:47:40 +0300

    Added class StreamDecorator with methods ReadFromStream and WriteToStream

--- List/StreamDecorator.cs
+++ List/StreamDecorator.cs
@@ -2,44 +2,77 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace List
 {
-    public class ReadWriteFromStream : Stream
+    public class StreamDecorator : Stream
     {
-        public override bool CanRead => throw new NotImplementedException();
+        readonly Stream inner;
 
-        public override bool CanSeek => throw new NotImplementedException();
+        public StreamDecorator(Stream inner)
+        {
+            this.inner = inner;
+        }
 
-        public override bool CanWrite => throw new NotImplementedException();
+        public override bool CanRead => inner.CanRead;
 
-        public override long Length => throw new NotImplementedException();
+        public override bool CanSeek => inner.CanSeek;
 
-        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override bool CanWrite => inner.CanWrite;
+
+        public override long Length => inner.Length;
+
+        public override long Position { get => inner.Position; set => inner.Position = value; }
 
         public override void Flush()
         {
-            throw new NotImplementedException();
+            inner.Flush();
         }
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            throw new NotImplementedException();
+            return inner.Read(buffer, offset, count);
         }
 
         public override long Seek(long offset, SeekOrigin origin)
         {
-            throw new NotImplementedException();
+            return inner.Seek(offset, origin);
         }
 
         public override void SetLength(long value)
         {
-            throw new NotImplementedException();
+            inner.SetLength(value);
         }
 
         public override void Write(byte[] buffer, int offset, int count)
         {
-            throw new NotImplementedException();
+            inner.Write(buffer, offset, count);
+        }
+
+        public void ReadFromStream(string path, bool gzip = false, bool encrypt = false)
+        {
+            StreamReader reader = new StreamReader(path);
+            string? line = reader.ReadLine();
+
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = reader.ReadLine();
+            }
+
+            reader.Close();
+        }
+
+        public void WriteToStream(string path, bool gzip = false, bool encrypt = false)
+        {
+            StreamWriter writer = new StreamWriter(path);
+
+            string? text = Console.ReadLine();
+
+            writer.WriteLine(text);
+
+            writer.Close();
         }
     }
 }

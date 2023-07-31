f16354ca42f9a37a27fdaed324a01733c763d89f
RaulFit <fitraul6@gmail.com>
2023-07-31 15:33:43 +0300

    Modified class StreamDecorator

--- List/StreamDecorator.cs
+++ List/StreamDecorator.cs
@@ -4,75 +4,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace List
+namespace GenericList
 {
-    public class StreamDecorator : Stream
+    public class StreamDecorator
     {
-        readonly Stream inner;
-
-        public StreamDecorator(Stream inner)
+        public void WriteToStream(Stream stream, string text, bool gzip = false, bool encrypt = false)
         {
-            this.inner = inner;
-        }
-
-        public override bool CanRead => inner.CanRead;
-
-        public override bool CanSeek => inner.CanSeek;
-
-        public override bool CanWrite => inner.CanWrite;
-
-        public override long Length => inner.Length;
-
-        public override long Position { get => inner.Position; set => inner.Position = value; }
-
-        public override void Flush()
-        {
-            inner.Flush();
-        }
-
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            return inner.Read(buffer, offset, count);
-        }
-
-        public override long Seek(long offset, SeekOrigin origin)
-        {
-            return inner.Seek(offset, origin);
-        }
-
-        public override void SetLength(long value)
-        {
-            inner.SetLength(value);
-        }
-
-        public override void Write(byte[] buffer, int offset, int count)
-        {
-            inner.Write(buffer, offset, count);
-        }
-
-        public void ReadFromStream(string path, bool gzip = false, bool encrypt = false)
-        {
-            StreamReader reader = new StreamReader(path);
-            string? line = reader.ReadLine();
-
-            while (line != null)
+            if (stream == null)
             {
-                Console.WriteLine(line);
-                line = reader.ReadLine();
+                throw new ArgumentNullException("Stream cannot be null.");
             }
 
-            reader.Close();
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            stream.Write(bytes, 0, bytes.Length);
         }
 
-        public void WriteToStream(string path, bool gzip = false, bool encrypt = false)
+        public string ReadFromStream(Stream stream, bool gzip = false, bool encrypt = false)
         {
-            StreamWriter writer = new StreamWriter(path);
+            if (stream == null)
+            {
+                throw new ArgumentNullException("Stream cannot be null.");
+            }
 
-            string? text = Console.ReadLine();
+            stream.Position = 0;
 
-            writer.WriteLine(text);
+            string text;
 
-            writer.Close();
+            var reader = new StreamReader(stream);
+
+            text = reader.ReadToEnd();
+
+            return text;
         }
     }
 }

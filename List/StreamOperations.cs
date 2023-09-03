a0912e8699619627edc2a5cc17b62e76e927c336
FitRaul <91602738+FitRaul@users.noreply.github.com>
2023-09-03 19:49:11 +0300

    Modified class StreamOperations

--- List/StreamOperations.cs
+++ List/StreamOperations.cs
@@ -13,7 +13,7 @@ namespace StreamDecorator
         byte[] emptySalt = Array.Empty<byte>();
         private byte[] compressed;
         private string key;
-        private readonly byte[] iv = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
+        private readonly byte[] iv = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
 
         public StreamOperations(string key = "")
         {
@@ -30,7 +30,6 @@ namespace StreamDecorator
             if (gzip && !encrypt)
             {
                 var bytes = Encoding.UTF8.GetBytes(text);
-
                 using (var msi = new MemoryStream(bytes))
                 {
                     using (var gs = new GZipStream(stream, CompressionMode.Compress, true))
@@ -68,11 +67,11 @@ namespace StreamDecorator
 
             if (gzip && !encrypt)
             {
-                using (var msi = new MemoryStream(compressed))
                 using (var mso = new MemoryStream())
                 {
-                    using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                    using (var gs = new GZipStream(stream, CompressionMode.Decompress))
                     {
+                        stream.Position = 0;
                         gs.CopyTo(mso);
                     }
 

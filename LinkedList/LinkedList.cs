34565c450d66d82ef2e7b25bdc9b3f9903a75103
FitRaul <91602738+FitRaul@users.noreply.github.com>
2023-09-12 14:20:15 +0300

    Refactorized LinkedList<T>

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -6,15 +6,7 @@ namespace MyLinkedList
 {
     public class LinkedList<T> : ICollection<T>
     {
-        public int Count { get; private set; }
-
-        public bool IsReadOnly { get; private set; }
-
         private Node<T> sentinel;
-
-        public Node<T> First { get; private set; }
-
-        public Node<T> Last { get; private set; }
 
         public LinkedList()
         {
@@ -22,51 +14,50 @@ namespace MyLinkedList
             sentinel.Next = sentinel.Prev = sentinel;
         }
 
+        public int Count { get; private set; }
+
+        public bool IsReadOnly { get; private set; }
+
+        public Node<T>? First
+        {
+            get => sentinel.Next;
+        }
+
+        public Node<T>? Last
+        {
+            get => sentinel.Prev;
+        }
+
         public void AddFirst(Node<T> node)
         {
-            First = sentinel;
-            node.Next = First.Next;
-            node.Prev = First;
-            First.Next.Prev = node;
-            First.Next = node;
-            First = node;
+            node.Next = sentinel.Next;
+            node.Prev = sentinel;
+            sentinel.Next.Prev = node;
+            sentinel.Next = node;
             Count++;
         }
 
         public Node<T> AddFirst(T data)
         {
             Node<T> node = new Node<T>(data);
-            First = sentinel;
-            node.Next = First.Next;
-            node.Prev = First;
-            First.Next.Prev = node;
-            First.Next = node;
-            First = node;
-            Count++;
+            AddFirst(node);
             return node;
         }
 
         public void AddLast(Node<T> node)
         {
-            Last = sentinel.Prev;
-            node.Next = Last.Next;
-            node.Prev = Last;
-            Last.Next.Prev = node;
-            Last.Next = node;
-            Last = node;
+            Node<T> last = sentinel.Prev;
+            node.Next = last.Next;
+            node.Prev = last;
+            last.Next.Prev = node;
+            last.Next = node;
             Count++;
         }
 
         public Node<T> AddLast(T data)
         {
             Node<T> node = new Node<T>(data);
-            Last = sentinel.Prev;
-            node.Next = Last.Next;
-            node.Prev = Last;
-            Last.Next.Prev = node;
-            Last.Next = node;
-            Last = node;
-            Count++;
+            AddLast(node);
             return node;
         }
 
@@ -91,22 +82,8 @@ namespace MyLinkedList
 
         public Node<T> AddAfter(Node<T> prevNode, T value)
         {
-            if (prevNode == default)
-            {
-                return default;
-            }
-
             Node<T> newNode = new Node<T>(value);
-            newNode.Next = prevNode.Next;
-            prevNode.Next = newNode;
-            newNode.Prev = prevNode;
-
-            if (newNode.Next != default)
-            {
-                newNode.Next.Prev = newNode;
-            }
-
-            Count++;
+            AddAfter(prevNode, newNode);
             return newNode;
         }
 
@@ -121,14 +98,9 @@ namespace MyLinkedList
             nextNode.Prev = newNode;
             newNode.Next = nextNode;
 
-            if(newNode.Prev != default)
+            if (newNode.Prev != default)
             {
                 newNode.Prev.Next = newNode;
-            }
-
-            else
-            {
-                First = newNode;
             }
 
             Count++;
@@ -137,103 +109,108 @@ namespace MyLinkedList
         public Node<T> AddBefore(Node<T> nextNode, T value)
         {
             Node<T> newNode = new Node<T>(value);
-
-            if (nextNode == default)
-            {
-                return default;
-            }
-
-            newNode.Prev = nextNode.Prev;
-            nextNode.Prev = newNode;
-            newNode.Next = nextNode;
-
-            if (newNode.Prev != default)
-            {
-                newNode.Prev.Next = newNode;
-            }
-
-            else
-            {
-                First = newNode;
-            }
-
-            Count++;
+            AddBefore(nextNode, newNode);
             return newNode;
         }
 
-        public Node<T>? Find(T value)
+        public Node<T>? Find(T value, bool last = false)
         {
             Node<T> node;
-            sentinel.Data = value;
-            for (node = sentinel.Next; !node.Data.Equals(value); node = node.Next)
-            {
-            }
-
-            sentinel.Data = default;
-            if (node == sentinel)
-            {
-                return default;
-            }
-
-            return node;
-        }
-
-        public Node<T>? FindLast(T value)
-        {
-            Node<T> node;
-            Node<T> lastNode = null;
+            Node<T> returnNode = default;
             sentinel.Data = value;
             for (node = sentinel.Next; node != sentinel; node = node.Next)
             {
-                if (node.Data.Equals(value))
+                if(node.Data.Equals(value) && !last)
                 {
-                    lastNode = node;
+                    returnNode = node;
+                    break;
+                }
+
+                if (node.Data.Equals(value) && last)
+                {
+                    returnNode = node;
                 }
             }
 
             sentinel.Data = default;
-            if (lastNode == sentinel)
+            if (returnNode == sentinel)
             {
                 return default;
             }
 
-            return lastNode;
+            return returnNode;
+        }
+
+        public Node<T>? FindLast(T value)
+        {
+            return Find(value, true);
         }
 
         void ICollection<T>.Add(T item)
         {
-            Node<T> node = new Node<T>(item);
-            Last = sentinel.Prev;
-            node.Next = Last.Next;
-            node.Prev = Last;
-            Last.Next.Prev = node;
-            Last.Next = node;
-            Last = node;
-            Count++;
+            AddLast(item);
         }
 
         public void Clear()
         {
             sentinel.Next = sentinel.Prev = sentinel;
-            First = Last = sentinel;
             Count = 0;
         }
 
         public bool Contains(T item)
         {
-            Node<T> node;
-            sentinel.Data = item;
-            for (node = sentinel.Next; !node.Data.Equals(item); node = node.Next)
-            {
-            }
+            return Find(item) != default;
+        }
 
-            sentinel.Data = default;
-            if (node == sentinel)
+        public bool Remove(Node<T> specifiedNode)
+        {
+            if (!Contains(specifiedNode.Data))
             {
                 return false;
             }
 
+            Node<T> current;
+
+            for (current = sentinel.Next; !current.Data.Equals(specifiedNode.Data); current = current.Next)
+            {
+            }
+
+            current.Prev.Next = current.Next;
+            current.Next.Prev = current.Prev;
+            Count--;
             return true;
+        }
+
+        public bool Remove(T item)
+        {
+            Node<T> nodeToRemove = new Node<T>(item);
+            return Remove(nodeToRemove);
+        }
+
+        public void RemoveFirst()
+        {
+            if (sentinel.Next == sentinel)
+            {
+                return;
+            }
+
+            Node<T> first = sentinel.Next;
+            sentinel.Next = first.Next;
+            sentinel.Next.Prev = sentinel;
+            Count--;
+        }
+
+        public void RemoveLast()
+        {
+            if(sentinel.Prev == sentinel)
+            {
+                return;
+            }
+
+            Node<T> last = sentinel.Prev;
+            sentinel.Prev = last.Prev;
+            sentinel.Prev.Next = sentinel;
+            Count--;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
@@ -259,78 +236,6 @@ namespace MyLinkedList
                 array[i + arrayIndex] = current.Data;
                 current = current.Next;
             }
-        }
-
-        public bool Remove(T item)
-        {
-            if (!Contains(item))
-            {
-                return false;
-            }
-
-            Node<T> current;
-
-            for (current = sentinel.Next; !current.Data.Equals(item); current = current.Next)
-            {
-            }
-
-            current.Prev.Next = current.Next;
-            current.Next.Prev = current.Prev;
-            First = sentinel.Next;
-            Last = sentinel.Prev;
-            Count--;
-            return true;
-        }
-
-        public bool Remove(Node<T> specifiedNode)
-        {
-            if (!Contains(specifiedNode.Data))
-            {
-                return false;
-            }
-
-            Node<T> current;
-
-            for (current = sentinel.Next; !current.Data.Equals(specifiedNode.Data); current = current.Next)
-            {
-            }
-
-            current.Prev.Next = current.Next;
-            current.Next.Prev = current.Prev;
-            First = sentinel.Next;
-            Last = sentinel.Prev;
-            Count--;
-            return true;
-        }
-
-        public void RemoveFirst()
-        {
-            if (sentinel.Next == sentinel)
-            {
-                return;
-            }
-
-            Node<T> first = sentinel.Next;
-            sentinel.Next = first.Next;
-            sentinel.Next.Prev = sentinel;
-            First = sentinel.Next;
-            Last = sentinel.Prev;
-            Count--;
-        }
-
-        public void RemoveLast()
-        {
-            if(sentinel.Prev == sentinel)
-            {
-                return;
-            }
-
-            Node<T> last = sentinel.Prev;
-            sentinel.Prev = last.Prev;
-            sentinel.Prev.Next = sentinel;
-            Last = sentinel.Prev;
-            First = sentinel.Next;
-            Count--;
         }
 
         public IEnumerator<T> GetEnumerator()

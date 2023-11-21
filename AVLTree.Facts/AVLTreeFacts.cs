2f07a4131c943c881fabf19baea86568e9a5d4e5
RaulFit <fitraul6@gmail.com>
2023-11-21 22:05:13 +0200

    Implemented Insert() and IsBalanced() methods

--- AVLTree.Facts/AVLTreeFacts.cs
+++ AVLTree.Facts/AVLTreeFacts.cs
@@ -8,5 +8,38 @@ namespace MyAVLTree.Facts
             var tree = new AVLTree();
             Assert.Null(tree.Find(3));
         }
+
+        [Fact]
+        public void Insert_ShouldCreateAndInsertNode()
+        {
+            var tree = new AVLTree();
+            tree.Insert(3);
+            Assert.NotNull(tree.Find(3));
+        }
+
+        [Fact]
+        public void Insert_WorkWhenAddingMultipleNodes()
+        {
+            var tree = new AVLTree();
+            tree.Insert(1);
+            tree.Insert(2);
+            tree.Insert(3);
+            Assert.NotNull(tree.Find(1));
+            Assert.NotNull(tree.Find(2));
+            Assert.NotNull(tree.Find(3));
+        }
+
+        [Fact]
+        public void TreeSelfBalancesWhenInsertingMaxValueNodes()
+        {
+            var tree = new AVLTree();
+            tree.Insert(1);
+            tree.Insert(2);
+            tree.Insert(3);
+            tree.Insert(4);
+            tree.Insert(5);
+            tree.Insert(6);
+            Assert.True(tree.IsBalanced());
+        }
     }
 }
--- AVLTree/AVLTree.cs
+++ AVLTree/AVLTree.cs
@@ -4,36 +4,137 @@ namespace MyAVLTree
 {
     public class AVLTree
     {
-        private Node? Root;
+        private Node? root;
 
-        public Node? Find(int key)
+        public Node? Find(int value)
         {
-            Node? current = Root;
+            Node? current = root;
             while (current != null)
             {
-                if (current.Key == key)
+                if (current.value == value)
                 {
                     break;
                 }
 
-                current = current.Key < key ? current.Right : current.Left;
+                current = current.value < value ? current.right : current.left;
             }
 
             return current;
         }
+
+        public void Insert(int value) => root = Insert(value, root);
+
+        public bool IsBalanced() => IsBalanced(root);
+
+        private Node Insert(int value, Node? node)
+        {
+            if (node == null)
+            {
+                node = new Node(value);
+                return node;
+            }
+
+            if (value < node.value)
+            {
+                node.left = Insert(value, node.left);
+            }
+
+            if (value > node.value)
+            {
+                node.right = Insert(value, node.right);
+            }
+
+            updateHeight(node);
+            return Rotate(node);
+        }
+
+        private Node Rotate(Node node)
+        {
+            if (Height(node.left) - Height(node.right) > 1)
+            {
+                if (Height(node.left.left) - Height(node.left.right) > 0)
+                {
+                    return rightRotate(node);
+                }
+
+                if (Height(node.left.left) - Height(node.left.right) < 0)
+                {
+                    node.left = leftRotate(node.left);
+                    return rightRotate(node);
+                }
+            }
+
+            if (Height(node.left) - Height(node.right) < -1)
+            {
+                if (Height(node.right.left) - Height(node.right.right) < 0)
+                {
+                    return leftRotate(node);
+                }
+
+                if (Height(node.right.left) - Height(node.right.right) > 0)
+                {
+                    node.right = rightRotate(node.right);
+                    return leftRotate(node);
+                }
+            }
+
+            return node;
+        }
+
+        private bool IsBalanced(Node? node)
+        {
+            if (node == null)
+            {
+                return true;
+            }
+
+            return Math.Abs(Height(node.left) - Height(node.right)) <= 1 && IsBalanced(node.left) && IsBalanced(node.right);
+        }
+
+        private Node rightRotate(Node node)
+        {
+            Node? left = node.left;
+            Node? leftRight = left?.right;
+
+            left.right = node;
+            node.left = leftRight;
+
+            updateHeight(node);
+            updateHeight(left);
+
+            return left;
+        }
+
+        private Node leftRotate(Node node)
+        {
+            Node? right = node.right;
+            Node? rightLeft = right?.left;
+
+            right.left = node;
+            node.right = rightLeft;
+
+            updateHeight(right);
+            updateHeight(node);
+
+            return right;
+        }
+
+        private int Height(Node? node) => node == null ? -1 : node.height;
+        
+        private void updateHeight(Node node) => node.height = Math.Max(Height(node.left), Height(node.right)) + 1;
     }
 
     public sealed class Node
     {
-        public int Key;
-        public int Height;
-        public Node? Left;
-        public Node? Right;
+        public int value;
+        public int height;
+        public Node? left;
+        public Node? right;
 
 
-        public Node(int key)
+        public Node(int value)
         {
-            Key = key;
+            this.value = value;
         }
     }
 }

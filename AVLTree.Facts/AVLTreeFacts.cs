0e7d83cd9eaff7bfd46eda6c5376ae604f3f4107
RaulFit <fitraul6@gmail.com>
2023-11-24 01:56:48 +0200

    Implemented method Insert()

--- AVLTree.Facts/AVLTreeFacts.cs
+++ AVLTree.Facts/AVLTreeFacts.cs
@@ -8,5 +8,27 @@ namespace MyAVLTree.Facts
             var tree = new AVLTree<int>();
             Assert.Null(tree.Find(1));
         }
+
+        [Fact]
+        public void Insert_ShouldInsertNodeWithSpecifiedValue()
+        {
+            var tree = new AVLTree<int>();
+            tree.Insert(1);
+            tree.Insert(2);
+            tree.Insert(3);
+            Assert.NotNull(tree.Find(1));
+            Assert.NotNull(tree.Find(2));
+            Assert.NotNull(tree.Find(3));
+        }
+
+        [Fact]
+        public void Insert_ShouldThrowExceptionWhenTreeAlreadyContainsNodeWithSpecifiedValue()
+        {
+            var tree = new AVLTree<int>();
+            tree.Insert(1);
+            tree.Insert(2);
+            tree.Insert(3);
+            Assert.Throws<InvalidOperationException>(() => tree.Insert(2));
+        }
     }
 }
--- AVLTree/AVLTree.cs
+++ AVLTree/AVLTree.cs
@@ -14,18 +14,101 @@
                     break;
                 }
 
-                else if (value.CompareTo(current.value) < 0)
-                {
-                    current = current.left;
-                }
-
-                else
-                {
-                    current = current.right;
-                }
+                current = value.CompareTo(current.value) < 0 ? current.left : current.right;
             }
 
             return current;
+        }
+
+        public void Insert(T value) => root = Insert(root, value);
+
+        private Node<T>? Insert(Node<T>? root, T value)
+        {
+            if (root == null)
+            {
+                return new Node<T>(value);
+            }
+
+            else if (value.CompareTo(root.value) < 0)
+            {
+                root.left = Insert(root.left, value);
+            }
+
+            else if (value.CompareTo(root.value) > 0)
+            {
+                root.right = Insert(root.right, value);
+            }
+
+            else
+            {
+                throw new InvalidOperationException("Tree already contains the specified value");
+            }
+
+            return Balance(root);
+        }
+
+        private Node<T>? Balance(Node<T> root)
+        {
+            UpdateHeight(root);
+            int balance = GetBalance(root);
+
+            if (balance > 1)
+            {
+                if (GetHeight(root?.left?.left) - GetHeight(root?.left?.right) > 0)
+                {
+                    root = RotateRight(root);
+                }
+
+                if (GetHeight(root?.left?.left) - GetHeight(root?.left?.right) < 0)
+                {
+                    root.left = RotateLeft(root.left);
+                    root = RotateRight(root);
+                }
+            }
+
+            else if (balance < 1)
+            {
+                if (GetHeight(root?.right?.right) - GetHeight(root?.right?.left) > 0)
+                {
+                    root = RotateLeft(root);
+                }
+
+                if (GetHeight(root?.right?.right) - GetHeight(root?.right?.left) < 0)
+                {
+                    root.right = RotateRight(root.right);
+                    root = RotateLeft(root);
+                }
+            }
+
+            return root;
+        }
+
+        public int GetHeight(Node<T>? node) => node != null ? node.height : -1;
+
+        private void UpdateHeight(Node<T> node) => node.height = 1 + Math.Max(GetHeight(node.left), GetHeight(node.right));
+
+        private int GetBalance(Node<T> node) => node == null ? 0 : GetHeight(node.left) - GetHeight(node.right);
+
+        private Node<T> RotateLeft(Node<T>? y)
+        {
+            Node<T>? x = y.right;
+            Node<T>? z = x?.left;
+            x.left = y;
+            y.right = z;
+            UpdateHeight(y);
+            UpdateHeight(x);
+            return x;
+        }
+
+        private Node<T> RotateRight(Node<T>? y)
+        {
+            Node<T>? x = y.left;
+            Node<T>? z= x?.right;
+            x.right = y;
+            y.left = z;
+            UpdateHeight(y);
+            UpdateHeight(x);
+            return x;
         }
 
         public sealed class Node<T> where T : IComparable<T>

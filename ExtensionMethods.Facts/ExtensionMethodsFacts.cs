b2c5bace46e0d2f4db5e3865c8bdbee35ce49958
RaulFit <fitraul6@gmail.com>
2023-11-28 16:16:53 +0200

    Implemented extension methods All() and Any()

---
+++ ExtensionMethods.Facts/ExtensionMethodsFacts.cs
@@ -0,0 +1,29 @@
+namespace ExtensionMethods.Facts
+{
+    public class ExtensionMethodsFacts
+    {
+        [Fact]
+        public void All_ShouldReturnTrueWhenAllElementsRespectTheCondition()
+        {
+            Assert.True(ExtensionMethods.All(new int[] { 2, 4, 6, 8, 10 }, e => e % 2 == 0));
+        }
+
+        [Fact]
+        public void All_ShouldReturnFalseWhenNotAllElementsRespectTheCondition()
+        {
+            Assert.False(ExtensionMethods.All(new int[] { 2, 4, 6, 7, 10 }, e => e % 2 == 0));
+        }
+
+        [Fact]
+        public void Any_ShouldReturnTrueWhenAnElementRespectsTheCondition()
+        {
+            Assert.True(ExtensionMethods.Any(new int[] { 1 , 5 ,7 ,8 ,13 }, e => e % 2 == 0));
+        }
+
+        [Fact]
+        public void Any_ShouldReturnFalseWhenNoElementRespectsTheCondition()
+        {
+            Assert.False(ExtensionMethods.Any(new int[] { 1, 5, 7, 9, 13 }, e => e % 2 == 0));
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿namespace ExtensionMethods
+{
+    public static class ExtensionMethods
+    {
+        public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (!predicate(item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool Any<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}

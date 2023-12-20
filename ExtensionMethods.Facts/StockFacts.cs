2b2f25619568df60c55fd7df70a16e91559fdbe5
RaulFit <fitraul6@gmail.com>
2023-12-20 20:07:15 +0200

    Implemented method IsInStock()

---
+++ ExtensionMethods.Facts/StockFacts.cs
@@ -0,0 +1,12 @@
+﻿namespace ExtensionMethods.Facts
+{
+    public class StockFacts
+    {
+        [Fact]
+        public void IsInStock_ShouldReturnFalseWhenProductIsNotInStock()
+        {
+            var stock = new Stock();
+            Assert.False(stock.IsInStock("charger"));
+        }
+    }
+}
---
+++ ExtensionMethods/Stock.cs
@@ -0,0 +1,22 @@
+﻿namespace ExtensionMethods
+{
+    public class Stock
+    {
+        private Dictionary<string, int> products;
+
+        public Stock()
+        {
+            products = new Dictionary<string, int>();
+        } 
+
+        public bool IsInStock(string name)
+        {
+            if (products.TryGetValue(name, out int quantity))
+            {
+                return quantity > 0;
+            }
+
+            return false;
+        }
+    }
+}

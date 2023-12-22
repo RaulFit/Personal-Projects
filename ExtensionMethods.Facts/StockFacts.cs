5b325761cb8b92a14964b6df858f56e52d229405
FitRaul <91602738+FitRaul@users.noreply.github.com>
2023-12-22 14:33:08 +0200

    Added class Stock

--- ExtensionMethods.Facts/StockFacts.cs
+++ ExtensionMethods.Facts/StockFacts.cs
@@ -2,50 +2,6 @@
 {
     public class StockFacts
     {
-        [Fact]
-        public void IsInStock_ShouldReturnFalseWhenProductIsNotInStock()
-        {
-            var stock = new Stock();
-            Assert.False(stock.IsInStock("charger"));
-        }
-
-        [Fact]
-        public void Add_ShouldAddNewProductInStock()
-        {
-            var stock = new Stock();
-            stock.Add("charger", 10);
-            Assert.True(stock.IsInStock("charger"));
-        }
-
-        [Fact]
-        public void Add_ShoulThrowExceptionWhenStockAlreadyContainsTheSpecifiedProduct()
-        {
-            var stock = new Stock();
-            stock.Add("charger", 10);
-            Assert.Throws<ArgumentException>(() => stock.Add("charger", 4));
-        }
-
-        [Fact]
-        public void Add_ShoulThrowExceptionWhenQuantityIsLessThanZero()
-        {
-            var stock = new Stock();
-            Assert.Throws<ArgumentException>(() => stock.Add("charger", -2));
-        }
-
-        [Fact]
-        public void FillStock_ShoulThrowExceptionWhenSpecifiedProductIsNotInStock()
-        {
-            var stock = new Stock();
-            Assert.Throws<ArgumentException>(() => stock.FillStock("charger", 10));
-        }
-
-        [Fact]
-        public void FillStock_ShouldFillStockForSpecifiedProduct()
-        {
-            var stock = new Stock();
-            stock.Add("charger", 0);
-            stock.FillStock("charger", 10);
-            Assert.True(stock.IsInStock("charger"));
-        }
+        
     }
 }
--- ExtensionMethods/Stock.cs
+++ ExtensionMethods/Stock.cs
@@ -2,46 +2,23 @@
 {
     public class Stock
     {
-        private Dictionary<string, int> products;
+        private List<Product> products;
 
         public Stock()
         {
-            products = new Dictionary<string, int>();
+            this.products = new List<Product>();
         }
+    }
 
-        public void Add(string name, int quantity)
+    public sealed class Product
+    {
+        internal readonly string Name;
+        internal int Quantity;
+
+        public Product(string name, int quantity)
         {
-            if (products.ContainsKey(name))
-            {
-                throw new ArgumentException($"Stock already contains {name}");
-            }
-
-            if (quantity < 0)
-            {
-                throw new ArgumentException($"The quantity cannot be less than zero");
-            }
-
-            products.Add(name, quantity);
-        }
-
-        public void FillStock(string name, int quantity)
-        {
-            if (!products.ContainsKey(name))
-            {
-                throw new ArgumentException($"{name} is not in stock");
-            }
-
-            products[name] += quantity;
-        }
-
-        public bool IsInStock(string name)
-        {
-            if (products.TryGetValue(name, out int quantity))
-            {
-                return quantity > 0;
-            }
-
-            return false;
+            Name = name;
+            Quantity = quantity;
         }
     }
 }

71981b86206e2097eeab3b96ccfa8f9ca949fc66
FitRaul <91602738+FitRaul@users.noreply.github.com>
2023-12-22 14:47:49 +0200

    Implemented method IsInStock()

--- ExtensionMethods.Facts/StockFacts.cs
+++ ExtensionMethods.Facts/StockFacts.cs
@@ -2,6 +2,10 @@
 {
     public class StockFacts
     {
-        
+        public void IsInStock_ShouldReturnFalseWhenSpecifiedProductIsNotInStock()
+        {
+            var stock = new Stock();
+            Assert.False(stock.IsInStock("charger"));
+        }
     }
 }
--- ExtensionMethods/Stock.cs
+++ ExtensionMethods/Stock.cs
@@ -1,4 +1,6 @@
-﻿namespace ExtensionMethods
+﻿using System.Runtime.CompilerServices;
+
+namespace ExtensionMethods
 {
     public class Stock
     {
@@ -7,6 +9,29 @@
         public Stock()
         {
             this.products = new List<Product>();
+        }
+
+        public bool IsInStock(string product)
+        {
+            IsNull(product);
+
+            foreach (var prod in products)
+            {
+                if (prod.Name.Equals(product))
+                {
+                    return prod.Quantity > 0;
+                }
+            }
+
+            return false;
+        }
+
+        private void IsNull(string param, [CallerArgumentExpression(nameof(param))] string paramName = "")
+        {
+            if (param == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
         }
     }
 

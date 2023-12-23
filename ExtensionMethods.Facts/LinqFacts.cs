28e5c70a7bc3573412dc2f138a2ac69cde012ad8
FitRaul <91602738+FitRaul@users.noreply.github.com>
2023-12-23 19:00:38 +0200

    Implemented function CountVowelsAndConsonants(), which returns the number of vowels and consonants from a string

---
+++ ExtensionMethods.Facts/LinqFacts.cs
@@ -0,0 +1,38 @@
+﻿using Stock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.Facts
+{
+    public class LinqFacts
+    {
+        [Fact]
+        public void CountVowelsAndConsonants_StringIsNull_ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => Linq.CountVowelsAndConsonants(null));
+        }
+
+        [Fact]
+        public void CountVowelsAndConsonants_ShouldWorkCorrectlyWhenStrinIsEmpty()
+        {
+            Assert.Equal((0, 0), Linq.CountVowelsAndConsonants(""));
+        }
+
+        [Fact]
+        public void CountVowelsAndConsonants_StringIsEmpty_ShouldHaveZeroVowelsAndConsonants()
+        {
+            string word = "hello";
+            Assert.Equal((2, 3), Linq.CountVowelsAndConsonants(word));
+        }
+
+        [Fact]
+        public void CountVowelsAndConsonants_ShouldWorkCorrectlyWithUpperAndLowerCaseLetters()
+        {
+            string word = "HeLlO";
+            Assert.Equal((2, 3), Linq.CountVowelsAndConsonants(word));
+        }
+    }
+}
---
+++ ExtensionMethods/Linq.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public class Linq
+    {
+        public static (int vowels, int consonants) CountVowelsAndConsonants(string word)
+        {
+            IsNull(word);
+
+            string vowels = "aeiouAEIOU";
+            int vowelsCount = word.Count(vowels.Contains);
+            int consonantsCount = word.Count(e => !vowels.Contains(e));
+            return (vowelsCount, consonantsCount);
+        }
+
+        private static void IsNull(string param, [CallerArgumentExpression(nameof(param))] string paramName = "")
+        {
+            if (param == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+    }
+}

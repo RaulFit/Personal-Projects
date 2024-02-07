45e7914f7d953213e1b7676bde416f2cce7a59fb
RaulFit <fitraul6@gmail.com>
2024-02-07 16:17:18 +0200

    Created class TextEditor, which reads the content from a text file and displays it in the terminal

---
+++ TextEditor/TextEditor.cs
@@ -0,0 +1,22 @@
+﻿namespace Editor
+{
+    class TextEditor
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                throw new ArgumentNullException("The specified path does not exist. Please provide a correct path");
+            }
+
+            string filePath = args[0];
+
+            if (!File.Exists(filePath))
+            {
+                throw new ArgumentException("The specified path is incorrect or does not exist. Please provide a correct path");
+            }
+
+            Console.WriteLine(File.ReadAllText(filePath));
+        }
+    }
+}
